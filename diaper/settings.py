58e7b2d772e0e58bbba719bcedd7faf86f723542
matersg <67175563+matersg@users.noreply.github.com>
2020-06-20 14:40:57 +0800

    LOG_LEVEL: DEBUG => INFO

--- diaper/settings.py
+++ diaper/settings.py
@@ -100,3 +100,5 @@ HTTPCACHE_EXPIRATION_SECS = 60 * 60 * 3
 HTTPCACHE_DIR = 'httpcache'
 HTTPCACHE_IGNORE_HTTP_CODES = []
 HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+LOG_LEVEL = 'INFO'
