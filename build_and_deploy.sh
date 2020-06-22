612ccb709b98d90c8adbd1777e50043c900bdd79
matersg <67175563+matersg@users.noreply.github.com>
2020-06-22 08:59:57 +0800

    special commit message for auto-updates

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -8,5 +8,5 @@ scrapy crawl amazon -o amazon.csv
 scrapy crawl fairprice -o fairprice.csv
 python generate_md.py
 git add README.md
-git commit -m 'update README.md'
+git commit -m 'auto-update README.md'
 git -c core.sshCommand="ssh -i ~/.ssh/id_rsa_matersg -F /dev/null" push
