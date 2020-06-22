c3a2fcd2d98019f0bfe770b144ec88b0437666f9
matersg <67175563+matersg@users.noreply.github.com>
2020-06-22 20:29:18 +0800

    update spiders/fairprice
    - Parse product list page instead of crawling
      each product page
    - Move url definitions to csv
    - Temporarily ignore 'Out of stock' info. This
      info have not been parsed correctly anyway,
      even with scrapy_selenium

--- diaper/spiders/fairprice.py
+++ diaper/spiders/fairprice.py
@@ -1,49 +1,38 @@
 import datetime
-import re
 import scrapy
-from scrapy_selenium import SeleniumRequest
+import pandas as pd
 
 from diaper.items import DiaperItem
 
 class FairpriceSpider(scrapy.Spider):
     name = 'fairprice'
-    start_urls = [
-        'https://www.fairprice.com.sg/search?query=diapers&filter=brand%3Amamypoko%26Country%20Of%20Origin%3AJapan',
-        'https://www.fairprice.com.sg/search?query=diapers&filter=brand%3Amamypoko%26Country%20Of%20Origin%3AIndonesia',
-        'https://www.fairprice.com.sg/search?query=diapers&filter=brand%3Amamypoko%26Country%20Of%20Origin%3AThailand',
-        'https://www.fairprice.com.sg/search?query=diapers&filter=brand%3Amerries',
-        'https://www.fairprice.com.sg/search?query=diapers&filter=brand%3Apampers',
-    ]
+    start_urls = pd.read_csv('fairprice_urls.csv').url.to_list()
 
     def parse(self, response):
-        product_links = response.css('div.product-container a::attr(href)').getall()
-        for url in product_links:
-            full_url = response.urljoin(url)
-            # Use headless chrome to get 'Out of stock' info
-            yield SeleniumRequest(
-                url=full_url,
-                callback=self.parse_product,
-            )
+        container_link_xpath = '//div[contains(@class, "product-container")]/a'
+        url_list = response.xpath(f'{container_link_xpath}/@href').getall()
 
-    def parse_product(self, response):
-        name = response.xpath('//span[@weight="regular"]/text()').get()
-        if name:
-            name = name.strip()
-        else:
-            return
+        description_xpath = f'{container_link_xpath}/div/div[2]'
+        price_list = response.xpath(f'{description_xpath}/div[1]/div/span[1]/span/text()').getall()
+        name_list  = response.xpath(f'{description_xpath}/div[2]/span/text()').getall()
+        units_list = response.xpath(f'{description_xpath}/div[2]/div/div/span[1]/text()').getall()
 
-        if response.xpath('//span[text()="Out of stock"]').get():
-            availability = 'Out of stock'
-        else:
-            availability = 'In stock'
-
-        yield DiaperItem(
-            name=name,
-            brand=response.xpath('//span[@data-testid="brandDetails"]/a/text()').get(),
-            units=response.xpath('//span[@data-testid="brandDetails"]/parent::div/span/span/text()').get(),
-            price=response.xpath('//span[@weight="black"]/text()').get().split('$')[1],
-            country=response.xpath('//h2[@title="Country of Origin"]/parent::div/div/span/text()').get(),
-            availability=availability,
-            url=response.request.url,
-            date_crawled=datetime.datetime.today(),
+        df = pd.DataFrame(
+            name_list,
+            columns=['name']
+        ).assign(
+            url=[response.urljoin(x) for x in url_list],
+            price=[x.strip('$') for x in price_list],
+            units=units_list,
         )
+        for r in df.itertuples():
+            yield DiaperItem(
+                name=r.name,
+                brand=None,
+                units=r.units,
+                price=r.price,
+                country=None,
+                availability=None, #FIXME!
+                url=r.url,
+                date_crawled=datetime.datetime.today(),
+            )
