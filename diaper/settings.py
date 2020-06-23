ec441e2ebbc494aa9e98d90de6698219f425134c
matersg <67175563+matersg@users.noreply.github.com>
2020-06-23 20:40:09 +0800

    fix scrapy_selenium setting
    This should address the issue
    "WARNING: Disabled SeleniumMiddleware:
     SELENIUM_DRIVER_NAME and
     SELENIUM_DRIVER_EXECUTABLE_PATH must be set"
    
    ... Actually scrapy_seleiumn was not working!! :-<

--- diaper/settings.py
+++ diaper/settings.py
@@ -46,10 +46,9 @@ DOWNLOAD_DELAY = 5
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-SPIDER_MIDDLEWARES = {
-    # 'diaper.middlewares.DiaperSpiderMiddleware': 543,
-    'scrapy_selenium.SeleniumMiddleware': 800,
-}
+# SPIDER_MIDDLEWARES = {
+#     'diaper.middlewares.DiaperSpiderMiddleware': 543,
+# }
 
 from shutil import which
 
@@ -64,9 +63,9 @@ SELENIUM_DRIVER_ARGUMENTS=[
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'diaper.middlewares.DiaperDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_selenium.SeleniumMiddleware': 800,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
