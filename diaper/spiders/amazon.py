8d06734827d612b02b0ff8db75b714e18a1862ab
matersg <67175563+matersg@users.noreply.github.com>
2020-06-23 21:21:08 +0800

    Revert "update spiders/amazon"
    This reverts commit 7d8c864a8faa32e1e903a01fbb26590a2194b9b8.

--- diaper/spiders/amazon.py
+++ diaper/spiders/amazon.py
@@ -1,6 +1,6 @@
 import datetime
+import re
 import scrapy
-import pandas as pd
 from scrapy_selenium import SeleniumRequest
 
 from diaper.items import DiaperItem
@@ -9,7 +9,12 @@ class AmazonSpider(scrapy.Spider):
     name = 'amazon'
 
     def start_requests(self):
-        urls = pd.read_csv('amazon_urls.csv').url.to_list()
+        urls = [
+            'https://www.amazon.sg/s?k=diapers&rh=p_6%3AACT6OAM3OSC9S%2Cp_89%3APampers&dc&qid=1592445069&rnid=6966029051&ref=sr_nr_p_76_1',
+            'https://www.amazon.sg/s?k=diapers&rh=p_6%3AACT6OAM3OSC9S%2Cp_89%3AMerries&dc&qid=1592445347&rnid=6733559051&ref=sr_nr_p_89_3',
+            'https://www.amazon.sg/s?k=diapers&rh=p_6%3AACT6OAM3OSC9S%2Cp_89%3AMamyPoko&dc&qid=1592445367&rnid=6733559051&ref=sr_nr_p_89_4',
+            'https://www.amazon.sg/s?k=moony&i=hpc&rh=p_6%3AACT6OAM3OSC9S&dc&qid=1592447693&rnid=6469115051&ref=sr_nr_p_6_1',
+        ]
         for url in urls:
             # Use headless chrome because crawling from AWS was blocked
             yield SeleniumRequest(
@@ -18,6 +23,13 @@ class AmazonSpider(scrapy.Spider):
             )
 
     def parse(self, response):
+        product_links = response.xpath('//span[@cel_widget_id="MAIN-SEARCH_RESULTS"]').css('a.a-link-normal::attr(href)').getall()
+        for product_link in product_links:
+            yield SeleniumRequest(
+                url=response.urljoin(product_link),
+                callback=self.parse_product,
+            )
+
         next_links = response.xpath('//a[text()="Next"]/@href').getall()
         for next_link in next_links:
             yield SeleniumRequest(
@@ -25,32 +37,20 @@ class AmazonSpider(scrapy.Spider):
                 callback=self.parse,
             )
 
-        cell_xpath = '//span[@cel_widget_id="MAIN-SEARCH_RESULTS"]'
-        url_xpath = f'{cell_xpath}/div/div/div[2]/h2/a/@href'
-        url_list = response.xpath(url_xpath).getall()
-        name_xpath = f'{cell_xpath}/div/div/div[2]/h2/a/span/text()'
-        name_list = response.xpath(name_xpath).getall()
-        price_xpath = '//span[@class="a-price"]/span[1]/text()'
-        price_list = response.xpath(price_xpath).getall()
-        cell_inner_html_list = response.xpath(cell_xpath).getall()
+    def parse_product(self, response):
+        name = response.css('span#productTitle::text').get()
+        if name:
+            name = name.strip()
+        else:
+            return
 
-        NO_STOCK = 'Temporarily out of stock'
-        df = pd.DataFrame(
-            name_list,
-            columns=['name']
-        ).assign(
-            url=[response.urljoin(x) for x in url_list],
-            price=[x.strip('S$') for x in price_list],
-            availability=[(NO_STOCK if NO_STOCK in x else 'OK') for x in cell_inner_html_list],
+        yield DiaperItem(
+            name=name,
+            brand=response.css('a#bylineInfo::text').get(),
+            units=response.xpath('//td[text()="Units"]/following-sibling::td/text()').get(),
+            price=response.css('span#price_inside_buybox::text').get().strip().split('$')[1],
+            country=None,
+            availability=response.css('div#availability span::text').get().strip(),
+            url=response.request.url,
+            date_crawled=datetime.datetime.today(),
         )
-        for r in df.itertuples():
-            yield DiaperItem(
-                name=r.name,
-                brand=None,
-                units=None,
-                price=r.price,
-                country=None,
-                availability=r.availability,
-                url=r.url,
-                date_crawled=datetime.datetime.today(),
-            )
