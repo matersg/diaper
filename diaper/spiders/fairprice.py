cd65f503b8514a44d50aa466004e2e1013f3c1bd
sainoky <sainoky@gmail.com>
2020-11-21 20:37:26 +0800

    fairprice spider: bug fix to get correct units

--- diaper/spiders/fairprice.py
+++ diaper/spiders/fairprice.py
@@ -1,4 +1,5 @@
 import datetime
+from bs4 import BeautifulSoup
 import scrapy
 import pandas as pd
 
@@ -9,28 +10,19 @@ class FairpriceSpider(scrapy.Spider):
     start_urls = pd.read_csv('fairprice_urls.csv').url.to_list()
 
     def parse(self, response):
-        container_link_xpath = '//div[contains(@class, "product-container")]/a'
-        url_list   = response.xpath(f'{container_link_xpath}/@href').getall()
-        price_list = response.xpath(f'{container_link_xpath}/div/div[3]/div/div/span/span/text()').getall()
-        name_list  = response.xpath(f'{container_link_xpath}/div/div[3]/div[2]/span/text()').getall()
-        units = response.xpath(f'{container_link_xpath}/div/div[3]/div[2]/div/div/span/text()').get()
-
-        df = pd.DataFrame(
-            name_list,
-            columns=['name']
-        ).assign(
-            url=[response.urljoin(x) for x in url_list],
-            price=[x.strip('$') for x in price_list],
-            units=units,
-        )
-        for r in df.itertuples():
+        soup = BeautifulSoup(response.text, 'html.parser')
+        for c in soup.find_all('div', {'class': 'product-container'}):
+            price = c.select_one('span span').get_text()
+            name = c.find('span', {'weight': 'normal'}).get_text()
+            units = c.select_one('span:contains("per pack")').get_text()
+            url = c.find('a').get('href')
             yield DiaperItem(
-                name=r.name,
+                name=name,
                 brand=None,
-                units=r.units,
-                price=r.price,
+                units=units,
+                price=price,
                 country=None,
                 availability=None, #FIXME!
-                url=r.url,
+                url=response.urljoin(url),
                 date_crawled=datetime.datetime.today(),
             )
